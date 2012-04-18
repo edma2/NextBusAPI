3bddf8c54b859e5b843d3854ee6a3f156f897e66
Eugene Ma <edma2@san.rr.com>
2012-04-18 00:34:57 -0700

    for testing purposes, get a list of all stopIds

--- NextBusAPI.java
+++ NextBusAPI.java
@@ -11,6 +11,7 @@ import java.io.InputStreamReader;
 import java.io.BufferedReader;
 import java.io.Reader;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
 
 import org.xml.sax.XMLReader;
@@ -94,10 +95,16 @@ public class NextBusAPI {
     }
 
     public static void main(String[] args) {
+        HashSet<Integer> stopIds = new HashSet<Integer>();
         NextBusAPI api = new NextBusAPI("actransit");
+
         try {
             for (String route : api.getRouteList()) {
-                System.out.println(route);
+                for (Stop stop : api.getRouteConfig(route))
+                    stopIds.add(stop.stopId);
+            }
+            for (int stopId : stopIds) {
+                System.out.println(stopId);
             }
         } catch (IOException ex) {
             ex.printStackTrace();
