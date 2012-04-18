b051c782d07370f15c35cb049429e0151e26a303
Eugene Ma <edma2@san.rr.com>
2012-04-17 18:54:05 -0700

    added Route class

---
+++ Route.java
@@ -0,0 +1,16 @@
+import java.util.LinkedList;
+
+public class Route {
+    public String direction;
+    public String title;
+    public LinkedList<Integer> predictions = new LinkedList<Integer>();
+
+    public Route(String direction, String title) {
+        this.direction = direction;
+        this.title = title;
+    }
+
+    public void addPrediction(int seconds) {
+        predictions.add(seconds);
+    }
+}
