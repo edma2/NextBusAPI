787ce8adbb1fcc53f6bc311f73a5624bf8e5b413
Eugene Ma <edma2@san.rr.com>
2012-04-17 19:08:26 -0700

    directly parse responses

--- API.java
+++ API.java
@@ -9,19 +9,30 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.BufferedReader;
+import java.io.Reader;
 import java.util.HashMap;
+import java.util.LinkedList;
+
+import org.xml.sax.XMLReader;
+import org.xml.sax.SAXException;
+import org.xml.sax.helpers.XMLReaderFactory;
+import org.xml.sax.InputSource;
 
 public class API {
     private final String URL = "http://webservices.nextbus.com/service/publicXMLFeed";
 
-    public String getPredictions(int stopId) throws IOException {
+    public LinkedList<Route> getRoutes(int stopId) throws IOException, SAXException {
         HashMap<String, String> params = new HashMap<String, String>();
         params.put("command", "predictions");
         params.put("a", "actransit");
         params.put("stopId", Integer.toString(stopId));
         String url = urlFromParams(params);
-        String xml = retrieve(url);
-        return xml;
+        XMLReader xr = XMLReaderFactory.createXMLReader();
+        Parser parser = new Parser();
+        xr.setContentHandler(parser);
+        xr.setErrorHandler(parser);
+        xr.parse(new InputSource(retrieve(url)));
+        return parser.routes;
     }
 
     /**
@@ -38,31 +49,28 @@ public class API {
     /**
      * Returns the resource located at a the given URL with query string
      * parameters. The data received is assumed to be encoded as UTF-8 by
-     * default, and is returned as a String. Returns null on any response code
-     * except 200.
+     * default, and a Reader pointing to it is returned.
      */
-    private String retrieve(String url) throws IOException {
+    private Reader retrieve(String url) throws IOException {
         HttpURLConnection conn = (HttpURLConnection)new URL(url).openConnection();
         conn.setRequestMethod("GET");
         conn.setDoOutput(true);
         conn.connect();
-        if (!conn.getResponseMessage().equals("OK"))
-            return null;
-        BufferedReader reader = new BufferedReader(new InputStreamReader(conn.getInputStream(), "UTF8"));
-        String response = "";
-        String line;
-        while ((line = reader.readLine()) != null)
-            response += (line + "\n");
-        return response;
+        return new BufferedReader(new InputStreamReader(conn.getInputStream(), "UTF8"));
     }
 
     public static void main(String[] args) {
         API api = new API();
         try {
-            String xml = api.getPredictions(57776);
-            System.out.println(xml);
+            for (Route r : api.getRoutes(57776)) {
+                System.out.println(r.title + "->" + r.direction);
+                for (int seconds : r.predictions)
+                    System.out.println(seconds + " seconds left");
+            }
         } catch (IOException ex) {
             ex.printStackTrace();
+        } catch (SAXException ex1) {
+            ex1.printStackTrace();
         }
     }
 }
