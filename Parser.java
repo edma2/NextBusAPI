cdb151f27c2309a43e1155426ffe57073cd5b6c3
Eugene Ma <edma2@san.rr.com>
2012-04-17 19:08:20 -0700

    removed scaffold code

--- Parser.java
+++ Parser.java
@@ -2,9 +2,7 @@ import java.io.IOException;
 import java.util.LinkedList;
 
 import org.xml.sax.Attributes;
-import org.xml.sax.XMLReader;
 import org.xml.sax.SAXException;
-import org.xml.sax.helpers.XMLReaderFactory;
 import org.xml.sax.helpers.DefaultHandler;
 
 public class Parser extends DefaultHandler {
@@ -35,24 +33,5 @@ public class Parser extends DefaultHandler {
             java.lang.String qName) throws SAXException {
         if (localName.equals("predictions"))
             routes.add(currentRoute);
-    }
-
-    public static void main(String[] args) {
-        try {
-            XMLReader xr = XMLReaderFactory.createXMLReader();
-            Parser parser = new Parser();
-            xr.setContentHandler(parser);
-            xr.setErrorHandler(parser);
-            xr.parse("test.xml");
-            for (Route r : parser.routes) {
-                System.out.println(r.title + "->" + r.direction);
-                for (int seconds : r.predictions)
-                    System.out.println(seconds);
-            }
-        } catch (IOException ex1) {
-            ex1.printStackTrace();
-        } catch (SAXException ex2) {
-            ex2.printStackTrace();
-        }
     }
 }
