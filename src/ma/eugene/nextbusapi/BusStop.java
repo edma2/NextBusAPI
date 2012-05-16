78777d4178ecccf05d883131596c2e1c2965e9e1
Eugene Ma <edma2@san.rr.com>
2012-05-16 14:54:31 -0700

    moved distance formula method

--- src/ma/eugene/nextbusapi/BusStop.java
+++ src/ma/eugene/nextbusapi/BusStop.java
@@ -28,22 +28,4 @@ public class BusStop {
         API api = new API(agency);
         return api.getPredictions(stopId);
     }
-
-    /**
-     * Calculates the great-circle distance between two points using the
-     * Haversine forumla.
-     *
-     * @return  distance in meters
-     */
-    private double distance(int lat1, int long1, int lat2, int long2) {
-        int earthRadius = 6372797;
-        double dlat = Math.toRadians(lat2-lat1);
-        double dlong = Math.toRadians(long2-long1);
-        double a = Math.sin(dlat/2) * Math.sin(dlat/2) +
-            Math.cos(Math.toRadians(lat1)) * Math.cos(Math.toRadians(lat2)) *
-            Math.sin(dlong/2) * Math.sin(dlong/2);
-        double c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
-        double d = earthRadius * c;
-        return d;
-    }
 }
