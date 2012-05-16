c8d56fe577c704ed88b5b6bced4a96a216537905
Eugene Ma <edma2@san.rr.com>
2012-05-16 13:29:22 -0700

    updated scaffold

--- src/ma/eugene/nextbusapi/TestDrive.java
+++ src/ma/eugene/nextbusapi/TestDrive.java
@@ -6,33 +6,39 @@ import java.util.HashMap;
 import org.xml.sax.SAXException;
 
 public class TestDrive {
-    public static void main(String[] args) {
-        try {
-            BusStop stop = new BusStop("actransit", 54080, 1337, 1337);
-            System.out.println(stop);
-        } catch (IOException ex) {
-            ex.printStackTrace();
-        } catch (SAXException ex1) {
-            ex1.printStackTrace();
-        }
-    }
-
     /*
     public static void main(String[] args) {
-        HashMap<Integer, String> stops = new HashMap<Integer, String>();
-        API api = new API("actransit");
         try {
-            for (String route : api.getRouteList()) {
-                for (RouteConfigInfo stop : api.getRouteConfig(route))
-                    stops.put(stop.stopId, Float.toString(stop.latitude) + ','
-                            + Float.toString(stop.longitude));
+            API api = new API("actransit");
+            for (RouteConfigInfo stop : api.getRouteConfig("B")) {
+                System.out.println(stop.stopId);
+                System.out.println(stop.latitude);
+                System.out.println(stop.longitude);
             }
-            for (int stopId : stops.keySet())
-                System.out.println(stopId + ',' + stops.get(stopId));
+            //BusStop stop = new BusStop("actransit", 54080, 1337, 1337);
+            //System.out.println(stop);
         } catch (IOException ex) {
             ex.printStackTrace();
         } catch (SAXException ex1) {
             ex1.printStackTrace();
         }
     }*/
+
+    public static void main(String[] args) {
+        HashMap<Integer, String> stops = new HashMap<Integer, String>();
+        API api = new API("actransit");
+        try {
+            for (String route : api.getRouteList()) {
+                for (RouteConfigInfo stop : api.getRouteConfig(route))
+                    stops.put(stop.stopId, Double.toString(stop.latitude) + ","
+                            + Double.toString(stop.longitude));
+            }
+            for (int stopId : stops.keySet())
+                System.out.println(stopId + "," + stops.get(stopId));
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        } catch (SAXException ex1) {
+            ex1.printStackTrace();
+        }
+    }
 }
