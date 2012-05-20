291cd65c0f69b1e1654943ddcb59a4d03d25649b
Eugene Ma <edma2@san.rr.com>
2012-05-20 01:32:15 -0700

    added saving and loading files

--- src/ma/eugene/nextbus/NextBus.java
+++ src/ma/eugene/nextbus/NextBus.java
@@ -1,6 +1,9 @@
 package ma.eugene.nextbus;
 
 import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.File;
 import java.util.HashMap;
 import java.util.List;
 import java.util.LinkedList;
@@ -20,14 +23,6 @@ public abstract class NextBus {
     public NextBus(String agency) throws IOException, SAXException {
         api = new API(agency);
         collectStops(); /* might take some time */
-    }
-
-    public NextBus(String agency, String s) {
-        api = new API(agency);
-        for (String line : s.split("\n")) {
-            BusStop bs = new BusStop(api, line);
-            stops.put(bs.getStopId(), bs);
-        }
     }
 
     private void collectStops() throws IOException, SAXException {
@@ -58,12 +53,37 @@ public abstract class NextBus {
         return predictions;
     }
 
+    /** Serialization of state as a string. */
     public String toString() {
         StringBuilder sb = new StringBuilder();
         for (BusStop bs : stops.values())
             sb.append(bs + "\n");
         if (sb.charAt(sb.length()-1) == '\n')
             sb.deleteCharAt(sb.length()-1);
+        return sb.toString();
+    }
+
+    /** Loading state from file. */
+    public NextBus(String agency, String dumpPath) throws IOException {
+        api = new API(agency);
+        String s = readFromFile(new File(dumpPath));
+        for (String line : s.split("\n")) {
+            BusStop bs = new BusStop(api, line);
+            stops.put(bs.getStopId(), bs);
+        }
+    }
+
+    private String readFromFile(File file) throws IOException {
+        StringBuilder sb = new StringBuilder();
+        BufferedReader in = new BufferedReader(new FileReader(file));
+        try {
+            char[] buf = new char[1024];
+            int bytesRead = 0;
+            while ((bytesRead = in.read(buf)) >= 0)
+                sb.append(buf, 0, bytesRead);
+        } finally {
+            in.close();
+        }
         return sb.toString();
     }
 }
