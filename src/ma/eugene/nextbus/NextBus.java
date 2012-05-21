8f65e0cdc978fa37fe7143a93a6bf17ec537eedc
Eugene Ma <edma2@san.rr.com>
2012-05-21 00:33:03 -0700

    check length before accessing chars

--- src/ma/eugene/nextbus/NextBus.java
+++ src/ma/eugene/nextbus/NextBus.java
@@ -54,7 +54,7 @@ public abstract class NextBus {
         StringBuilder sb = new StringBuilder();
         for (BusStop bs : stops.values())
             sb.append(bs + "\n");
-        if (sb.charAt(sb.length()-1) == '\n')
+        if (sb.length() > 0)
             sb.deleteCharAt(sb.length()-1);
         return sb.toString();
     }
