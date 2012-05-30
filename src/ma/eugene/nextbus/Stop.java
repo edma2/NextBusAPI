72d47c206c66d6299f8c9886666b9d76ba0b6715
Eugene Ma <edma2@san.rr.com>
2012-05-29 22:16:31 -0700

    extend

--- src/ma/eugene/nextbus/Stop.java
+++ src/ma/eugene/nextbus/Stop.java
@@ -1,18 +1,12 @@
 package ma.eugene.nextbus;
 
-public class Stop {
-    public String title;
-    public String tag;
+public class Stop extends TaggedTitle {
     public double latitude;
     public double longitude;
     // TODO: stopId
 
-    public Stop() {
-    }
-
     public Stop(String tag, String title, double latitude, double longitude) {
-        this.tag = tag;
-        this.title = title;
+        super(tag, title);
         this.latitude = latitude;
         this.longitude = longitude;
     }
