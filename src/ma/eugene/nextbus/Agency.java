322adde98ba800565a545b4fe49d97a11a3a2d6e
Eugene Ma <edma2@san.rr.com>
2012-05-29 21:24:47 -0700

    added Agency and Route wrapper classes

---
+++ src/ma/eugene/nextbus/Agency.java
@@ -0,0 +1,11 @@
+package ma.eugene.nextbus;
+
+public class Agency {
+    public String tag;
+    public String title;
+
+    public Agency(String tag, String title) {
+        this.tag = tag;
+        this.title = title;
+    }
+}
---
+++ src/ma/eugene/nextbus/Route.java
@@ -0,0 +1,14 @@
+package ma.eugene.nextbus;
+
+public class Route {
+    public String tag;
+    public String title;
+
+    public Route() {
+    }
+
+    public Route(String tag, String title) {
+        this.tag = tag;
+        this.title = title;
+    }
+}
